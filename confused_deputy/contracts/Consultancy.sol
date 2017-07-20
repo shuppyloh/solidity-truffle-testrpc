992614f1a8e074998d3cb352c82c790507e0af71
Jordan <jpl216@ic.ac.uk>
2017-07-20 11:51:53 +0100

    update

--- confused_deputy/contracts/Consultancy.sol
+++ confused_deputy/contracts/Consultancy.sol
@@ -1,23 +1,25 @@
 pragma solidity ^0.4.11;
-//Abstract Inbox that Consultancy uses
+//Abstract Inbox that Consultant references for the save function 
 contract Inbox{
-    function save(string _data);
+    function save(bytes32 data1, bytes32 data2);
 }
 
 contract Customer is Inbox{
-    string message;
+    bytes32 consultant;
+    bytes32 product;
     function Customer(){}
     //the save function is public and accessible by everyone
-    function save(string _data){ message = _data; }
+    function save(bytes32 _consultant, bytes32 _product){ consultant = _consultant; product = _product; }
 }
 
 contract Bill is Inbox{
     address owner;
-    string billing;
+    bytes32 customer; 
+    bytes32 bill;
     modifier ownerOnly(){ require(msg.sender == owner);_;}
     function Bill(){ owner = msg.sender; }
     //the save function call can only be triggered by the owner(Consultant)
-    function save(string _data) ownerOnly { billing = _data; }
+    function save(bytes32 _customer, bytes32 _bill){ customer = _customer; bill = _bill;} 
 }
 
 contract Consultant{
@@ -27,12 +29,12 @@ contract Consultant{
         owner = msg.sender;
         myBill = _bill;
     }
-    function hire(address _customer, string _price){
-        //the consultant is supposed to bill the customer in a separate contract
-        Inbox(myBill).save(_price); 
-        //then deliver the work into the customer ...
-        Inbox(_customer).save("Work done for you");
-        //confused deputy problem arises when a malicious customer passes the address of the myBill as _customer
+    function hire(address _customerInbox) payable {
+        //the consultant bills the customer in a separate contract
+        Inbox(myBill).save(bytes32(msg.sender), bytes32(msg.value));
+        //then delivers the work into the customer ...
+        Inbox(_customerInbox).save(bytes32(address(this)), bytes32("Work done for you"));
+        //confused deputy problem arises when a customer passes the address of myBill as _customerInbox
         //even though the customer cannot call save on myBill, this is done through the confused deputy (Consultancy)
         //the consultant overwrites his bill with the work intended for the customer
     }
